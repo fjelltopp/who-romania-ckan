c51db4b3e699fb7b16eba8678a3c381eafcaa741
toavina <toavina@fjelltopp.org>
2023-09-25 10:01:36 +0300

    WRC 18 -

--- util/fd_data_loader/resource_loader.py
+++ util/fd_data_loader/resource_loader.py
@@ -29,7 +29,7 @@ MONTHS = {
 }
 
 
-def mutable_dataset_dict(title, name, month, resource=[], **kwargs):
+def mutable_dataset_dict(title, name, month, resources, **kwargs):
     return {
         "title": title,
         "name": name,
@@ -42,7 +42,7 @@ def mutable_dataset_dict(title, name, month, resource=[], **kwargs):
         "groups": [{"name": "family-medicine"}],
         "tags": [{"name": "Data"}],
         "year": kwargs.get("year", 2023),
-        "resources": resource
+        "resources": resources
     }
 
 
@@ -76,56 +76,60 @@ def traverse_folder_tree():
     return files
 
 
-def read_resource_file(file):
+def read_resource_sheet(workbook, sheet):
+    active = workbook[sheet]
+    report_date = active['B2'].value
+    fd_name = sheet.split('FD ')[1]
+
+    if not isinstance(report_date, datetime):
+        logging.warning(f"Report date {report_date} is not a date.")
+        print("Failed to use report date, attempting to load from report period ...")
+        report_date = active['B3'].value
+        if not isinstance(report_date, datetime):
+            logging.error(f"Report date {report_date} is not a date.")
+
+    if report_date.weekday() != 4:
+        logging.info(f"Report date {report_date} is not a Friday.")
+
+    year = str(report_date.year % 100)
+    month = "{:02}".format(report_date.month)
+    day = "{:02}".format(report_date.day)
+    week = year + '-' + month + '-' + day
+    week_number = str(report_date.isocalendar()[1])
+
+    new_folder_path = os.path.join(root_dir, "resources/family-medicine-reports", month, week_number, sheet)
+
+    template_workbook = load_workbook(root_dir + "/template.xlsx")
+    template_sheet = template_workbook.active
+
+    start_row, end_row = 6, 41
+    start_col, end_col = 1, 23  # A to W
+
+    for row in range(start_row, end_row + 1):
+        for col in range(start_col, end_col + 1):
+            cell_value = active.cell(row=row, column=col).value
+            template_sheet.cell(row=row, column=col, value=cell_value)
+
+    if not os.path.exists(new_folder_path):
+        os.makedirs(new_folder_path)
+
+    template_workbook.save(new_folder_path + '/report.xlsx')
+
+    return mutable_resource_dict("Report from Family Doctor " + fd_name + " for week number " + week_number,
+                                 'report.xlsx',
+                                 week,
+                                 fd_name)
+
+
+def load_file_sheets(file):
     print("Reading file: ", file)
     workbook = load_workbook(file)
     sheets = [item for item in workbook.sheetnames if item.startswith('FD')]
     sheets.sort()
-
+    all_resources = []
     for sheet in sheets:
-        active = workbook[sheet]
-        report_date = active['B2'].value
-        fd_name = sheet.split('FD ')[1]
-
-        if not isinstance(report_date, datetime):
-            logging.warning(f"Report date {report_date} is not a date.")
-            print("Failed to use report date, attempting to load from report period ...")
-            report_date = active['B3'].value
-            if not isinstance(report_date, datetime):
-                logging.error(f"Report date {report_date} is not a date.")
-                continue
-
-        if report_date.weekday() != 4:
-            logging.info(f"Report date {report_date} is not a Friday.")
-
-        year = str(report_date.year % 100)
-        month = "{:02}".format(report_date.month)
-        day = "{:02}".format(report_date.day)
-        week = year + '-' + month + '-' + day
-        week_number = str(report_date.isocalendar()[1])
-
-        new_folder_path = os.path.join(root_dir, "resources/family-medicine-reports", month, week_number, sheet)
-
-        template_workbook = load_workbook(root_dir + "/template.xlsx")
-        template_sheet = template_workbook.active
-
-        start_row, end_row = 6, 41
-        start_col, end_col = 1, 23  # A to W
-
-        for row in range(start_row, end_row + 1):
-            for col in range(start_col, end_col + 1):
-                cell_value = active.cell(row=row, column=col).value
-                template_sheet.cell(row=row, column=col, value=cell_value)
-
-        if not os.path.exists(new_folder_path):
-            os.makedirs(new_folder_path)
-
-        template_workbook.save(new_folder_path + '/report.xlsx')
-
-        return mutable_resource_dict("Report from Family Doctor " + fd_name + " for week number " + week_number,
-                                     'report.xlsx',
-                                     week,
-                                     fd_name)
+        all_resources.append(read_resource_sheet(workbook, sheet))
+    return all_resources
 
 
 def generate_dataset_dict():
@@ -133,14 +137,17 @@ def generate_dataset_dict():
         "datasets": []
     }
     fd_files = traverse_folder_tree()
-    for file in fd_files:
+    for file_dict in fd_files:
+        resources = []
+        for file in file_dict["files"]:
+            resources.extend(load_file_sheets(file))
         dataset_dict["datasets"].append(mutable_dataset_dict(
-            title="Family Medicine Reports" + " for " + MONTHS[int(file["month"])] + " " + file["year"],
-            name="family-medicine-reports" + "-" + (file["month"]) + "-" + (file["year"]),
-            month=file["year"] + "-"+ file["month"],
-            year=file["year"],
-            notes="WHO ROMANIA - Family Medicine Reports for " + MONTHS[int(file["month"])] + " " + file["year"],
-            resource=[read_resource_file(file) for file in file["files"]]
+            title="Family Medicine Reports" + " for " + MONTHS[int(file_dict["month"])] + " " + file_dict["year"],
+            name="family-medicine-reports" + "-" + (file_dict["month"]) + "-" + (file_dict["year"]),
+            month=file_dict["year"] + "-" + file_dict["month"],
+            year=file_dict["year"],
+            notes="WHO ROMANIA - Family Medicine Reports for " + MONTHS[int(file_dict["month"])] + " " + file_dict["year"],
+            resources=resources
         ))
 
     with open(root_dir + '/resources/datasets.json', 'w') as json_file:
